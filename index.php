92556380d58fc3ea5ffd0c8e3823dc1cdd3e1754
Tina <hristinaamarjanovic@gmail.com>
2025-08-02 01:32:34 +0200

    prvi commit

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
-    //echo "Cao";
+//echo "Cao";
 
-    include "katalog.php";
-?>
+
+
+
+$proizvodi = array(
+    array(
+        "id" => 1,
+        "naziv" => 'Laptop',
+        "cena" => 1000
+    ),
+    array(
+        "id" => 2,
+        "naziv" => 'Mis',
+        "cena" => 30
+    ),
+    array(
+        "id" => 3,
+        "naziv" => 'Tastatura',
+        "cena" => 100
+    )
+
+);
+
+include "katalog.php";
--- katalog.php
+++ katalog.php
@@ -21,37 +21,19 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>Laptop</td>
-                <td>500</td>
-                <td>
-                    <form action="" method="post">
-                        <input type="hidden" name="id" value="id-1">
-                        <input type="submit" name="submit" value="Kupi">
-                    </form>
-                </td>
-            </tr>
+            <?php foreach ($proizvodi as $pr): ?>
+                <tr>
+                    <td><?php echo $pr["naziv"] ?></td>
+                    <td> <?php echo $pr["cena"] ?></td>
+                    <td>
+                        <form action="" method="post">
+                            <input type="hidden" name="id" value="id-1">
+                            <input type="submit" name="submit" value="Kupi">
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
 
-            <tr>
-                <td>Kisobran</td>
-                <td>200</td>
-                <td>
-                    <form action="" method="post">
-                        <input type="hidden" name="id" value="id-2">
-                        <input type="submit" name="submit" value="Kupi">
-                    </form>
-                </td>
-            </tr>
-            <tr>
-                <td>Lopta</td>
-                <td>100</td>
-                <td>
-                    <form action="" method="post">
-                        <input type="hidden" name="id" value="id-3">
-                        <input type="submit" name="submit" value="Kupi">
-                    </form>
-                </td>
-            </tr>
 
         </tbody>
     </table>
